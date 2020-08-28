05cb858a3ca3196af4fef9630b426b25813b5dfa
Jason <37859597+zachowj@users.noreply.github.com>
2020-08-28 08:10:25 +0000

    Build geckodriver from source for arm

--- .docker/geckodriver.sh
+++ .docker/geckodriver.sh
@@ -1,8 +1,8 @@
-#!/bin/bash    
+#!/bin/bash
 GECKODRIVER_VERSION=$(curl https://github.com/mozilla/geckodriver/releases/latest | grep -Po 'v[0-9]+.[0-9]+.[0-9]+')
 arch=$(uname -m)
 if [[ $arch == x86_64* ]]; then
-    arch="linux64"    
+    arch="linux64"
 elif [[ $arch == i*86 ]]; then
     arch="linux32"
 elif  [[ $arch == arm* ]]; then
@@ -13,8 +13,28 @@ else
     exit 1
 fi
 
-URL="https://github.com/mozilla/geckodriver/releases/download/$GECKODRIVER_VERSION/geckodriver-$GECKODRIVER_VERSION-$arch.tar.gz"
+if [[ $arch == linux* ]]; then
+    URL="https://github.com/mozilla/geckodriver/releases/download/$GECKODRIVER_VERSION/geckodriver-$GECKODRIVER_VERSION-$arch.tar.gz"
+    curl -s -L "$URL" | tar -xz
+    chmod +x geckodriver
+    mv geckodriver /tmp
+else
+    apt-get -y install gcc-arm-linux-gnueabihf libc6-armhf-cross libc6-dev-armhf-cross
+    curl https://sh.rustup.rs -sSf | sh -s -- -y
+    source $HOME/.cargo/env
+    rustup target install armv7-unknown-linux-gnueabihf
 
-curl -s -L "$URL" | tar -xz
-chmod +x geckodriver
-mv geckodriver /tmp
+cat >>~/.cargo/config <<EOF
+[target.armv7-unknown-linux-gnueabihf]
+linker = "arm-linux-gnueabihf-gcc"
+EOF
+
+    export PATH="$PATH:$HOME/.cargo/bin"
+    cd $HOME
+    URL="https://github.com/mozilla/geckodriver/archive/$GECKODRIVER_VERSION.tar.gz"
+    curl -s -L "$URL" | tar -xz
+    cd geckodriver-${GECKODRIVER_VERSION#?}
+    cargo build --release --target armv7-unknown-linux-gnueabihf
+    find . -name geckodriver -print0 | xargs -0 -I {} mv {} /tmp
+    chmod +x /tmp/geckodriver
+fi
