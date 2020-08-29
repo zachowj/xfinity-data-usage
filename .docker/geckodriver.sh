4dc3a50404d12bd3fffc963c505ee69221d6741f
Jason <37859597+zachowj@users.noreply.github.com>
2020-08-29 05:46:57 +0000

    Revert build script

--- .docker/geckodriver.sh
+++ .docker/geckodriver.sh
@@ -1,35 +1,30 @@
 #!/bin/bash
+GECKODRIVER_VERSION=$(curl https://github.com/mozilla/geckodriver/releases/latest | grep -Po 'v[0-9]+.[0-9]+.[0-9]+')
 arch=$(uname -m)
 if [[ $arch == x86_64* ]]; then
-    GECKODRIVER_VERSION=$(curl https://github.com/mozilla/geckodriver/releases/latest | grep -Po 'v[0-9]+.[0-9]+.[0-9]+')
     URL="https://github.com/mozilla/geckodriver/releases/download/$GECKODRIVER_VERSION/geckodriver-$GECKODRIVER_VERSION-linux64.tar.gz"
     curl -s -L "$URL" | tar -xz
     chmod +x geckodriver
     mv geckodriver /tmp
 elif  [[ $arch == armv7* ]]; then
-    # https://firefox-source-docs.mozilla.org/testing/geckodriver/ARM.html
-    sudo apt install -y gcc-arm-linux-gnueabihf libc6-armhf-cross libc6-dev-armhf-cross
-    git clone --depth=1 https://github.com/mozilla/gecko-dev
-    curl https://sh.rustup.rs -sSf | bash -s -- -v -y
-    declare -a bindirs=("$HOME/.cargo/bin")
-    if [[ "$(cat $HOME/.bashrc | grep -P "^PATH=" )" == "" ]]; then
-    echo -e "PATH=\$PATH\n" >> $HOME/.bashrc
-    fi
-    for binfolder in "${bindirs[@]}"
-    do
-    if [[ "$(cat $HOME/.bashrc | grep -P "^PATH=" | grep "${binfolder}:")" == "" ]]; then
-        sed -i -e "s/^PATH=\"\(.*\)\"/PATH=\"$(echo "$binfolder" | sed "s/\//\\\\\//g"):\1\"/g" $HOME/.bashrc # place with no spaces in it
-        export PATH="$PATH:$binfolder"
-    fi
-    done
-    source $HOME/.bashrc
+    apt-get -y install gcc-arm-linux-gnueabihf libc6-armhf-cross libc6-dev-armhf-cross
+    curl https://sh.rustup.rs -sSf | sh -s -- -y
+    source $HOME/.cargo/env
     rustup target install armv7-unknown-linux-gnueabihf
-    echo -e "[target.armv7-unknown-linux-gnueabihf]
-    linker = \"arm-linux-gnueabihf-gcc\"" > "$HOME/gecko-dev/testing/geckodriver/.cargo/config"
-    cd "$HOME/gecko-dev/testing/geckodriver"
+
+cat >>~/.cargo/config <<EOF
+[target.armv7-unknown-linux-gnueabihf]
+linker = "arm-linux-gnueabihf-gcc"
+EOF
+
+    export PATH="$PATH:$HOME/.cargo/bin"
+    cd $HOME
+    URL="https://github.com/mozilla/geckodriver/archive/$GECKODRIVER_VERSION.tar.gz"
+    curl -s -L "$URL" | tar -xz
+    cd geckodriver-${GECKODRIVER_VERSION#?}
     cargo build --release --target armv7-unknown-linux-gnueabihf
-    # test binary file with this command
-    $HOME/gecko-dev/target/armv7-unknown-linux-gnueabihf/release/geckodriver --version
+    find . -name geckodriver -print0 | xargs -0 -I {} mv {} /tmp
+    chmod +x /tmp/geckodriver
 else
     echo arch not supported: $arch
     exit 1
