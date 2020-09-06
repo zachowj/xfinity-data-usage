6453a780ea00596f3e47bd54c5358535a0fa6f7b
Jason <37859597+zachowj@users.noreply.github.com>
2020-09-06 00:44:59 +0000

    fix: correct firefox spelling

--- src/xfinity.ts
+++ src/xfinity.ts
@@ -149,7 +149,7 @@ export class Xfinity extends EventEmitter {
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36';
 
         const useragent = randomUseragent.getRandom(function (ua) {
-            return ['Chrome', 'Firefix'].includes(ua.browserName) && parseFloat(ua.browserVersion) >= 40;
+            return ['Chrome', 'Firefox'].includes(ua.browserName) && parseFloat(ua.browserVersion) >= 40;
         });
 
         return useragent ?? USERAGENT;
