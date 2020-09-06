e5836a92bd51dc4f13a3a915dcc84fadf0c0bbc2
Jason <37859597+zachowj@users.noreply.github.com>
2020-09-06 00:50:59 +0000

    refactor: Use only chrome useragent strings

--- src/xfinity.ts
+++ src/xfinity.ts
@@ -149,7 +149,7 @@ export class Xfinity extends EventEmitter {
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36';
 
         const useragent = randomUseragent.getRandom(function (ua) {
-            return ['Chrome', 'Firefox'].includes(ua.browserName) && parseFloat(ua.browserVersion) >= 40;
+            return ua.browserName === 'Chrome' && parseFloat(ua.browserVersion) >= 50;
         });
 
         return useragent ?? USERAGENT;
