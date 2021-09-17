87b653e4613c0351e2e84bead319121d87eff46d
Jason <37859597+zachowj@users.noreply.github.com>
2021-09-17 12:43:28 +0000

    refactor: Replace sync writeFile with async

--- src/password.ts
+++ src/password.ts
@@ -1,51 +1,47 @@
-import { readFileSync, writeFile } from 'fs';
+import { readFileSync } from 'fs';
+import { writeFile } from 'fs/promises';
 
+import { CONFIG_FOLDER } from './config.js';
 import logger from './logger.js';
 
-const CONFIG_FOLDER = process.env.CONFIG_FOLDER ?? '/config';
 const SUFFIX_FILE = CONFIG_FOLDER + '/pwsuffix';
 
 export default class Password {
-    private password: string;
-    private suffix: number | undefined;
+    #password: string;
+    #suffix?: number;
 
     constructor(password: string) {
-        this.password = password;
+        this.#password = password;
         this.readPasswordFile();
     }
 
     readPasswordFile(): void {
-        let suffix: number | undefined;
-
         try {
             const data = readFileSync(SUFFIX_FILE, 'utf-8');
-            suffix = parseInt(data, 10);
+            this.#suffix = parseInt(data, 10);
         } catch (e) {
             logger.error('Unable to load password file.');
         }
-
-        this.suffix = suffix;
     }
 
     getPassword(): string {
-        const suffix = this.suffix?.toString().padStart(3, '0') ?? '';
+        const suffix = this.#suffix?.toString().padStart(3, '0') ?? '';
 
-        return `${this.password}${suffix}`;
+        return `${this.#password}${suffix}`;
     }
 
     generatePassword(): string {
-        this.suffix = this.suffix !== undefined ? this.suffix + 1 : 0;
+        this.#suffix = this.#suffix !== undefined ? this.#suffix + 1 : 0;
         return this.getPassword();
     }
 
     async savePassword(): Promise<void> {
-        if (this.suffix === undefined) return;
+        if (this.#suffix === undefined) return;
 
-        await writeFile(SUFFIX_FILE, this.suffix.toString(), (error) => {
-            if (error) {
-                console.error(error);
-                console.error('Error trying to write password suffix to file.');
-            }
-        });
+        try {
+            await writeFile(SUFFIX_FILE, this.#suffix.toString());
+        } catch (error) {
+            logger.error(`Error trying to write password suffix to file. ${error}`);
+        }
     }
 }
